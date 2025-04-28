96820c2e36437f1664d7b4857b0366828e530ca3
akulkarthikeya006@gmail.com <akulkarthikeya006@gmail.com>
2025-04-28 15:11:02 +0530

    ca-1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ServiceCard from './components/ServiceCard'
-
+import data from '../data.json'
 function App() {
   const [service, setService] = useState(data)
 
